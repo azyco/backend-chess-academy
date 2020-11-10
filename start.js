d64aed5aa206a6e9205db48a4b2afc271c434b1c
nomeat1997 <namit.barik@gmail.com>
2020-11-10 18:36:21 +0530

    added service IP and port entry in config, changed IP from localhost to 0.0.0.0

--- start.js
+++ start.js
@@ -1,6 +1,6 @@
 const app = require('./app');
 const config = require('./config');
 
-const server = app.listen(config.servicePort, 'localhost', () => {
+const server = app.listen(config.servicePort, config.serviceIP, () => {
   console.log(`Express is running on port ${server.address().port}`);
 });
