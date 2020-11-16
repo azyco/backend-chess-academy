00ea0f0b70569035e84eca524e5bfde7addd290d
Rahul Ghose <hansum.rahul@gmail.com>
2020-11-16 20:56:29 +0530

    simple socketio server with express

--- start.js
+++ start.js
@@ -1,20 +1,25 @@
+/* eslint-disable no-console */
 const socketio = require('socket.io');
 const app = require('./app');
 
 const config = require('./config');
-
-// server.listen(config.servicePort, config.serviceIP, () => {
-//   // eslint-disable-next-line no-console
-//   console.log(`Express is running on port ${server.address().port}`);
-// });
 
 const server = app.listen(config.servicePort, config.serviceIP, () => {
   // eslint-disable-next-line no-console
   console.log(`Express is running on port ${server.address().port}`);
 });
 
+let sharedInMemoryObject = null;
+
 const io = socketio(server, { cors: true, origins: '*:*' });
 io.on('connection', (socket) => {
-  console.log('connect');
-  socket.send('test messgae');
+  if (sharedInMemoryObject) {
+    io.emit('board', sharedInMemoryObject);
+  }
+
+  socket.on('chat', (msg) => {
+    console.log('new chat message from UI', msg);
+    sharedInMemoryObject = msg;
+    io.emit('board', sharedInMemoryObject);
+  });
 });
